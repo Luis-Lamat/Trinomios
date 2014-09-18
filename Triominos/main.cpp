9c5e053cbe0cc02f978beef68ea9ceb04392bbdc
Luis-Lamat <beto_alaska@hotmail.com>
2014-09-18 18:48:21 -0500

    Initial commit

---
+++ Triominos/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Triominos
+//
+//  Created by Luis Alberto Lamadrid on 9/18/14.
+//  Copyright (c) 2014 Luis Alberto Lamadrid. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
